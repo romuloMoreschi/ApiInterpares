2cd107d524d6018bfb6ef369589aecb66fecd847
unknown <Romulo@cgi1050.indra.es>
2020-10-26 15:03:09 -0400

    Ajustando Token++

---
+++ ApiInterpares/Model/Grupo.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LoginApi.Models
+{
+    public class Grupo
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public List<Login> Logins { get; set; }
+    }
+}
---
+++ ApiInterpares/Model/LoginContext .cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace LoginApi.Models
+{
+    public class LoginContext : DbContext
+    {
+        public LoginContext(DbContextOptions<LoginContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Login> Login { get; set; }
+        public DbSet<Grupo> Grupo { get; set; }
+    }
+}
